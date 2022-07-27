411b7dec174eb9bc36380400b1c6ae71f8e031eb
Zakery Clarke <zakery@healthrhythms.com>
2022-07-27 16:35:21 -0400

    Update charts, logo etcc

--- src/Home.js
+++ src/Home.js
@@ -7,19 +7,39 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { red } from '@mui/material/colors';
 
-import { LinearProgressWithLabel } from "./components/LinearProgressWithLabel.js"
+import { Chart as ChartJS } from 'chart.js/auto'
+import { Chart }            from 'react-chartjs-2'
+import { Line } from 'react-chartjs-2';
 
+
+import { LinearProgressWithLabel } from "./components/LinearProgressWithLabel.js"
+import logo from "./assets/penguin_waving.gif"
 import './Home.css';
 
-import logo from "./assets/penguin_waving.gif"
+import { MentalHealthSDK } from "./sdk/MentalHealth.js"
+
+const mhsdk = new MentalHealthSDK()
+
+var data = {
+  labels:["1","2","3"],
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: [1,2,3],
+      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    }
+  ]
+}
 
 export default function Home() {
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <div>
+      <Card sx={{ m:2, maxWidth: 500 }} >
       <CardMedia
         component="img"
         alt="penguin waving"
-        height="100"
+        height="400"
         image={logo}
       />
       <CardContent>
@@ -34,9 +54,89 @@ export default function Home() {
       <LinearProgressWithLabel value={30} />
       <LinearProgressWithLabel value={60} />
       
+      <CardActions alignItems="center">
+        <Button variant="contained">Your Charts</Button>
+        <Button variant="contained">Your Data</Button>
+      </CardActions>
+    </Card>
+    <Card sx={{ m:2, maxWidth: 345, textAlign:"center" }}>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Survey Name Mental Health
+        </Typography>
+      </CardContent>
+
       <CardActions>
         <Button variant="contained">😔</Button><Button variant="contained">😐</Button><Button variant="contained">😊</Button>
       </CardActions>
     </Card>
+    <Card sx={{ m:2, maxWidth: 345, textAlign:"center" }}>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Survey Name Mental Health
+        </Typography>
+      </CardContent>
+
+      <CardActions>
+        <Button variant="contained">😔</Button><Button variant="contained">😐</Button><Button variant="contained">😊</Button>
+      </CardActions>
+    </Card>
+    <Card sx={{ m:2, maxWidth: 345, textAlign:"center" }}>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Survey Name Mental Health
+        </Typography>
+      </CardContent>
+
+      <CardActions>
+        <Button variant="contained">😔</Button><Button variant="contained">😐</Button><Button variant="contained">😊</Button>
+      </CardActions>
+    </Card>
+    <Card sx={{ m:2, maxWidth: 345, textAlign:"center" }}>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Survey Name Mental Health
+        </Typography>
+      </CardContent>
+
+      <CardActions>
+        <Button variant="contained">😔</Button><Button variant="contained">😐</Button><Button variant="contained">😊</Button>
+      </CardActions>
+    </Card>
+    <Card sx={{ m:2, maxWidth: 345, textAlign:"center" }}>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Survey Name Mental Health
+        </Typography>
+      </CardContent>
+
+      <CardActions>
+        <Button variant="contained">😔</Button><Button variant="contained">😐</Button><Button variant="contained">😊</Button>
+      </CardActions>
+    </Card>
+
+    <Card sx={{ m:2, maxWidth: 345, textAlign:"center" }}>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Sleep Schedule Name Mental Health
+        </Typography>
+        <Line data={data}
+        />
+      </CardContent>
+    </Card>
+    <Card sx={{ m:2, maxWidth: 345, textAlign:"center" }}>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+         Show me ze Data
+        </Typography>
+        <Typography gutterBottom variant="h5" component="div">{(JSON.stringify(mhsdk.datasrc).length/5000000)*100}%</Typography>
+        
+        <Typography gutterBottom variant="h5" component="div">{JSON.stringify(mhsdk.datasrc,null,2)}</Typography>
+        
+      </CardContent>
+    </Card>
+    </div>
+    
+    
   );
 }
--- src/sdk/MentalHealth.js
+++ src/sdk/MentalHealth.js
@@ -2,26 +2,60 @@ export class MentalHealthSDK {
 	constructor(config) {
 		this.config = {
             study_id:"default",
-            refresh_timeout:5*60*1000
+            refresh_timeout:1000,
+            surveys:[],
+            suggestions:[
+                {
+                    'title':"Sleep and Health",
+                    'text':"Make sure to get at least 6-8 hrs sleep tonight!"
+                }
+            ]
         };
 		this.datasrc = [];
-		
+        this._load_database()
+		this._init_sensors()
 	}
     _init_sensors(){
-        navigator.getBattery().then((data) => {
-            this._save_data
-        })
+        setInterval(()=>{this._data_loop()},this.config.refresh_timeout)
 	}
+    _data_loop(){
+        var self = this;
+        // navigator.getBattery().then((data) => {
+        //     self._save_data("battery",{
+        //         charging:data.charging,
+        //         batterylife:data.dischargingTime,
+        //         level:data.level
+        //     })
+        // })
+        // navigator.geolocation.getCurrentPosition((data) => {
+        //     self._save_data("gps",{
+        //         "latitude":data.coords.latitude,
+        //         "longitude":data.coords.longitude,
+        //         "altitude":data.coords.altitude,
+        //         "accuracy":data.coords.accuracy
+        //     })
+        // })
+        navigator.geolocation.getCurrentPosition((data) => {
+            console.log(data)
+            
+        })
+    }
     _save_database(){
-
+        localStorage.setItem("mha_raw", JSON.stringify(this.datasrc))
     }
 
     _load_database(){
-        
+        this.datasrc = JSON.parse(localStorage.getItem("mha_raw")||"[]")
     }
 
-    _save_data(params,update){
-        t
+    _save_data(label, data){
+        console.log(data);
+        this.datasrc.push({
+            'type':label,
+            'ts':(new Date()).getTime(),
+            'data':data
+        })
+        this._save_database()
 	}
 	features(params,update){
 	
@@ -29,22 +63,3 @@ export class MentalHealthSDK {
 	
 }
 
-class MushroomDB_Database(){
-	constructor(socket) {
-		this.database = [];
-	}
-	_load_db(){
-
-	}
-	_save_db(){
-
-	}
-
-	find(table,params){
-
-	}
-
-	_sendMessage(data) {
-		this.broadcast.emit("mushroomdb",data);
-	}
-}
